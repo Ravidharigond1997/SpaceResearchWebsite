48ccd05a7b92ff45292f2cce94444fd0cccc940a
Ravi Dharigond <66740454+Ravidharigond1997@users.noreply.github.com>
2023-09-23 19:20:34 +0530

    new changes updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,36 +5,38 @@ import styles from "../src/styles/navbar.module.css";
 
 function Navbar() {
   return (
-    <div className={styles.navbar}>
-      <div>
-        <Image
-          src="/rocket.png"
-          alt="logo"
-          width={90}
-          height={100}
-          className={styles.logo}
-        />
+    <>
+      <div className={styles.navbar}>
+        <div>
+          <Image
+            src="/rocket.png"
+            alt="logo"
+            width={90}
+            height={100}
+            className={styles.logo}
+          />
+        </div>
+        <div className={styles.navbardata}>
+          <ul className={styles.navbarlist}>
+            <Link href={"/"}>
+              <li className={styles.list}>Home</li>
+            </Link>
+            <Link href={"/achivements"}>
+              <li className={styles.list}>Achivements</li>
+            </Link>
+            <Link href={"/about"}>
+              <li className={styles.list}>About</li>
+            </Link>
+            <Link href={"/contact"}>
+              <li className={styles.list}>Contact</li>
+            </Link>
+          </ul>
+        </div>
+        <div className={styles.btn}>
+          <button className={styles.btn1}>Moreabout</button>
+        </div>
       </div>
-      <div className={styles.navbarlist}>
-        <ul className={styles.navbarlist}>
-          <Link href={"/"}>
-            <li>Home</li>
-          </Link>
-          <Link href={"/achivements"}>
-            <li>Achivements</li>
-          </Link>
-          <Link href={"/about"}>
-            <li>About</li>
-          </Link>
-          <Link href={"/contact"}>
-            <li>Contact</li>
-          </Link>
-        </ul>
-      </div>
-      <div className={styles.btn}>
-        <button className={styles.btn1}>Moreabout</button>
-      </div>
-    </div>
+    </>
   );
 }
 
--- src/pages/achivements.js
+++ src/pages/achivements.js
@@ -17,13 +17,13 @@ function Achivements() {
 
   return (
     <>
-      <div className={styles.container}>
+      {/* <div className={styles.container}>
         {achivements.map((data, key) => {
           return (
             <div key={key}>
               <div className={styles.card}>
                 <div className={styles.imageContainer}>
-                  <img src={data.image} alt="Avatar" />
+                  <img src={data.image} alt="Avatar" width="100%" />
                 </div>
                 <div className={styles.content}>
                   <div className={styles.title}>
@@ -44,6 +44,116 @@ function Achivements() {
             </div>
           );
         })}
+      </div> */}
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img
+              src="https://cdnuploads.aa.com.tr/uploads/Contents/2018/08/12/thumbs_b_c_375cae4f85367b0c22a9c90bfd127599.jpg?v=121905"
+              alt="Avatar"
+              width="100%"
+            />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <h3 className={styles.h3}>
+                <b>Mission Sun</b>
+              </h3>
+              <div className={styles.body}>
+                <p className={styles.p}>
+                  This sun mession our first mission in our origanization.Its
+                  take to 13 moonth to complated in our organization
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.btn}>
+            <button>
+              <a>View more</a>
+            </button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img
+              src="https://static.scientificamerican.com/sciam/cache/file/1CD9AF72-0B9B-4312-BE7FF8C7F3E3C3AA_source.jpg"
+              alt="Avatar"
+              width="100%"
+            />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <h3 className={styles.h3}>
+                <b>Mission Mars</b>
+              </h3>
+              <div className={styles.body}>
+                <p className={styles.p}>
+                  This mars mession our second mission in our origanization.Its
+                  take to 12 moonth to complated in our organization
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.btn}>
+            <button>
+              <a>View more</a>
+            </button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img
+              src="https://i.gadgets360cdn.com/large/ch3_rover_isro_1692852195660.jpg"
+              alt="Avatar"
+              width="100%"
+            />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <h3 className={styles.h3}>
+                <b>Mission moon</b>
+              </h3>
+              <div className={styles.body}>
+                <p className={styles.p}>
+                  This moon mession our third mission in our origanization.Its
+                  take to 13 moonth to complated in our organization
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.btn}>
+            <button>
+              <a>View more</a>
+            </button>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU6HP69h1oUU5O7vWcv5UVc500PIeovID6TGWAQM-AqI9KOosPF7N3Ks4fH-FeaK-WFns&usqp=CAU"
+              alt="Avatar"
+              width="100%"
+            />
+          </div>
+          <div className={styles.content}>
+            <div className={styles.title}>
+              <h3 className={styles.h3}>
+                <b>Mission Earth</b>
+              </h3>
+              <div className={styles.body}>
+                <p className={styles.p}>
+                  This moon mession our third mission in our origanization.Its
+                  take to 6 moonth to complated in our organization
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.btn}>
+            <button>
+              <a>View more</a>
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
